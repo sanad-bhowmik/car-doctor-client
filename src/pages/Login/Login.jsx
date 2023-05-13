76a863dd2e4e6ac921cc4cf1981c2688ae3ad8fe
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-05-13 15:54:08 +0600

    update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -27,9 +27,25 @@ const Login = () => {
             .then(result => {
                 const user = result.user;
                 console.log(user);
+                const loggedUser = {
+                    email: user.email
+                }
+                console.log(loggedUser);
                 navigate(from, { replace: true })
                 form.reset();
                 notify();
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(loggedUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log('jwt response', data);
+                        localStorage.setItem('car-access-token', data.token);
+                    })
             })
             .catch(error => console.log(error));
     }
